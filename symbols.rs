7d86665eac4515f04dfd2d122323ad1efb16317c
Muthu Krishnan <muthukrishnan06@gmail.com>
2018-12-13 14:57:03 +0530

    Update symbols.rs

--- symbols.rs
+++ symbols.rs
@@ -1,7 +1,79 @@
-// List of Currency Symbols
 
-trait Symbol
+// List of Currency Status
+enum status
 {
-
+    PRE_TRADING(String),
+    TRADING(String),
+    POST_TRADING(String),
+    END_OF_DAY(String),
+    HALT(String),
+    AUCTION_MATCH(String),
+    BREAK(String)
 }
 
+// List of Trade Duration
+enum Duration
+{
+    GTC(String),
+    IOC(String),
+    FOK(String
+}
+ 
+// List of Orders
+enum OrderSide
+{
+    Buy(String),
+    Sell(String)
+}
+
+// Rate Limit Intervals
+enum RateLimit
+{
+    Hours(String),
+    Minutes(String),
+    Seconds(String)
+}
+
+// Structure of Currency 
+struct Currency 
+{
+    symbol: String,
+    status: status, 
+    price: f64
+}
+
+// Structure of an order
+struct Order
+{
+    symbol: String,
+    order_id: u64, 
+    clientOrderid: String, 
+    exchange: String, 
+    price: f64,
+    qty: u64,
+    status: String,
+    execQty: u64, 
+    activeTime: Duration, 
+    type: String, 
+    action: OrderSide
+}
+  
+// Structue of Trades
+struct Trade
+{
+    symbol: String,
+    order_id: u64, 
+    clientOrderid: String, 
+    exchange: String, 
+    price: f64,
+    qty: u64,
+    status: String,
+    execQty: u64, 
+    activeTime: Duration, 
+    type: String, 
+    action: OrderSide
+}
+
+
+
+    
